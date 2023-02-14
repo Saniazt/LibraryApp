b28fcda02386a15ae0e8276a2e56952736a0eb7f
Saniazt <saniazt@gmail.com>
2023-02-14 21:37:35 +0200

    xls export reports availability

--- src/main/java/com/saniazt/springcourse/LibraryWithJPAandBootAndHibern/reports/ExcelReport.java
+++ src/main/java/com/saniazt/springcourse/LibraryWithJPAandBootAndHibern/reports/ExcelReport.java
@@ -1,43 +1,61 @@
 package com.saniazt.springcourse.LibraryWithJPAandBootAndHibern.reports;
 
-import java.io.File;
-import java.io.FileOutputStream;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import com.spire.data.table.DataTable;
+import com.spire.data.table.common.JdbcAdapter;
+import com.spire.xls.ExcelVersion;
+import com.spire.xls.Workbook;
+import com.spire.xls.Worksheet;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.sql.*;
+
 
 public class ExcelReport {
     public static void main(String[] args) {
+        //Create a Workbook object
+        Workbook wb = new Workbook();
+
+        //Get the first worksheet
+        Worksheet sheet = wb.getWorksheets().get(0);
+
+        //Create a DataTable object
+        DataTable dataTable = new DataTable();
+
+        //Connect to database
+        String url = "jdbc:postgresql://localhost:5432/library_app_jpa_hiber";
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/library_app_jpa_hiber", "postgres", "postgres");
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM book");
-            XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.createSheet("book");
-            Row row = sheet.createRow(0);
-            Cell cell = row.createCell(0);
-            cell.setCellValue("Title");
-            int i = 0;
-            while (rs.next()) {
-                Row row1 = sheet.createRow(i+1);
-                Cell cell1 = row1.createCell(0);
-                cell1.setCellValue(rs.getString("Title"));
-                i++;
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            try {
+                Connection conn = DriverManager.getConnection(url,"postgres","postgres");
+                Statement sta = conn.createStatement();
+
+                //Select table from the database
+                ResultSet resultSet = sta.executeQuery("select * from books");
+                JdbcAdapter jdbcAdapter = new JdbcAdapter();
+
+                //Export data from database to datatable
+                jdbcAdapter.fillDataTable(dataTable, resultSet);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            FileOutputStream out = new FileOutputStream(new File("ExportDatabaseTableToExcel.xlsx"));
-            workbook.write(out);
-            out.close();
-            System.out.println("Exported successfully...");
-            conn.close();
-        } catch (Exception e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        //Write datatable to the worksheet
+        sheet.insertDataTable(dataTable, true, 1, 1);
+
+        //Auto fit column width
+        sheet.getAllocatedRange().autoFitColumns();
+
+        //Save to an Excel file
+        wb.saveToFile("output/ExportToExcel.xlsx", ExcelVersion.Version2016);
     }
 }
