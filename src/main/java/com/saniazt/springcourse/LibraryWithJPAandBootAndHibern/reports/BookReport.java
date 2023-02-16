58a75acd1e9699bf80143f9d139f1b66a83be33a
saniazt <saniazt@gmail.com>
2023-02-16 15:00:38 +0200

    xls export saves directly to desktop

---
+++ src/main/java/com/saniazt/springcourse/LibraryWithJPAandBootAndHibern/reports/BookReport.java
@@ -0,0 +1,69 @@
+package com.saniazt.springcourse.LibraryWithJPAandBootAndHibern.reports;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.spire.data.table.DataTable;
+import com.spire.data.table.common.JdbcAdapter;
+import com.spire.xls.ExcelVersion;
+import com.spire.xls.Workbook;
+import com.spire.xls.Worksheet;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class BookReport {
+    public static void main(String[] args) {
+        //Create a Workbook object
+        Workbook wb = new Workbook();
+
+        //Get the first worksheet
+        Worksheet sheet = wb.getWorksheets().get(0);
+
+        //Create a DataTable object
+        DataTable dataTable = new DataTable();
+
+        //Connect to database
+        String url = "jdbc:postgresql://localhost:5432/library_app_jpa_hiber";
+
+            try {
+                Connection conn = DriverManager.getConnection(url,"postgres","postgres");
+                Statement sta = conn.createStatement();
+
+                //Select table from the database
+                ResultSet resultSet = sta.executeQuery("select * from book");
+                JdbcAdapter jdbcAdapter = new JdbcAdapter();
+
+                //Export data from database to datatable
+                jdbcAdapter.fillDataTable(dataTable, resultSet);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        //Write datatable to the worksheet
+        sheet.insertDataTable(dataTable, true, 1, 1);
+
+        //Auto fit column width
+        sheet.getAllocatedRange().autoFitColumns();
+
+        // Get the current user's home directory
+        String homeDir = System.getProperty("user.home");
+
+// Append the relative path to the desktop
+        String desktopPath = homeDir + "/Desktop";
+        //Save to an Excel file
+        //wb.saveToFile("util/ExportToExcel.xlsx", ExcelVersion.Version2016);
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm");
+        String formattedDate = myDateObj.format(myFormatObj);
+        wb.saveToFile(desktopPath+"/BookReport "+formattedDate+".xlsx", ExcelVersion.Version2016);
+
+    }
+}
--- src/main/java/com/saniazt/springcourse/LibraryWithJPAandBootAndHibern/reports/PeopleReport.java
+++ src/main/java/com/saniazt/springcourse/LibraryWithJPAandBootAndHibern/reports/PeopleReport.java
@@ -1,10 +1,5 @@
 package com.saniazt.springcourse.LibraryWithJPAandBootAndHibern.reports;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import com.spire.data.table.DataTable;
 import com.spire.data.table.common.JdbcAdapter;
 import com.spire.xls.ExcelVersion;
@@ -12,9 +7,10 @@ import com.spire.xls.Workbook;
 import com.spire.xls.Worksheet;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-
-public class ExcelReport {
+public class PeopleReport {
     public static void main(String[] args) {
         //Create a Workbook object
         Workbook wb = new Workbook();
@@ -28,22 +24,22 @@ public class ExcelReport {
         //Connect to database
         String url = "jdbc:postgresql://localhost:5432/library_app_jpa_hiber";
 
-            try {
-                Connection conn = DriverManager.getConnection(url,"postgres","postgres");
-                Statement sta = conn.createStatement();
+        try {
+            Connection conn = DriverManager.getConnection(url,"postgres","postgres");
+            Statement sta = conn.createStatement();
 
-                //Select table from the database
-                ResultSet resultSet = sta.executeQuery("select * from book");
-                JdbcAdapter jdbcAdapter = new JdbcAdapter();
+            //Select table from the database
+            ResultSet resultSet = sta.executeQuery("select * from person");
+            JdbcAdapter jdbcAdapter = new JdbcAdapter();
 
-                //Export data from database to datatable
-                jdbcAdapter.fillDataTable(dataTable, resultSet);
+            //Export data from database to datatable
+            jdbcAdapter.fillDataTable(dataTable, resultSet);
 
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         //Write datatable to the worksheet
@@ -59,7 +55,10 @@ public class ExcelReport {
         String desktopPath = homeDir + "/Desktop";
         //Save to an Excel file
         //wb.saveToFile("util/ExportToExcel.xlsx", ExcelVersion.Version2016);
-        wb.saveToFile(desktopPath+"/ExportToExcel.xlsx", ExcelVersion.Version2016);
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm");
+        String formattedDate = myDateObj.format(myFormatObj);
+        wb.saveToFile(desktopPath+"/PeopleReport "+formattedDate+".xlsx", ExcelVersion.Version2016);
 
     }
 }
